841dfb9b4e8e2cc3bb8f8ade1e8beb2c4d504fba
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-13 15:08:39 -0500

    viewport dragging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,31 +8,54 @@
 		id,
 		title: `Title ${id + 1}`,
 		content: `lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-		position: { x: 20, y: 20 + id * 120 },
+		position: { x: 0, y: id * 120 },
 	}))
-
-	let selection = setStore<Card["id"]>()
 
 	function updateCard(id: Card["id"], getNext: (current: Card) => Card) {
 		cards = cards.map((card) => (card.id === id ? getNext(card) : card))
 	}
+
+	let selection = setStore<Card["id"]>()
+
+	let viewportOffset = { x: 20, y: 20 }
+	let draggingViewport = false
 </script>
+
+<svelte:window
+	on:pointermove={(event) => {
+		if (draggingViewport) {
+			event.preventDefault()
+			viewportOffset.x += event.movementX
+			viewportOffset.y += event.movementY
+		}
+	}}
+	on:pointerup={() => {
+		draggingViewport = false
+	}}
+	on:blur={() => {
+		draggingViewport = false
+	}}
+/>
 
 <main
 	role="presentation"
-	class="absolute h-screen w-screen overflow-hidden"
+	class="absolute h-screen w-screen touch-none overflow-hidden"
 	on:pointerdown={(event) => {
 		if (event.target === event.currentTarget) {
+			event.preventDefault()
 			selection.clear()
+			draggingViewport = true
 		}
 	}}
 >
 	{#each cards as card}
+		{@const x = card.position.x + viewportOffset.x}
+		{@const y = card.position.y + viewportOffset.y}
 		<div
 			role="presentation"
 			data-selected={$selection.has(card.id)}
 			class="bg-base-800 border-base-700 max-w-64 data-[selected=true]:border-accent-500 absolute cursor-pointer overflow-clip rounded-md border shadow-lg"
-			style="transform: translate({card.position.x}px, {card.position.y}px);"
+			style="transform: translate({x}px, {y}px);"
 			on:pointerdown={(event) => {
 				event.preventDefault()
 				event.stopPropagation()
