9fff5092407076efe946c9a2ed6f41fa9558d6c5
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-18 10:54:27 -0500

    loading placeholder for auth button

--- src/app/MobileBottomNav.tsx
+++ src/app/MobileBottomNav.tsx
@@ -2,15 +2,16 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/clerk-react"
 import {
 	LucideClock,
 	LucideDices,
+	LucideLoader2,
 	LucideLogIn,
 	LucideUsers,
 	type LucideIcon,
 } from "lucide-react"
+import { twMerge } from "tailwind-merge"
 import { panel } from "~/styles/panel"
 import { ActiveLink } from "../components/ActiveLink"
 
 export function MobileBottomNav() {
-	const { user, isLoaded } = useUser()
 	return (
 		<footer className={panel("sticky bottom-0 mt-auto border-t shadow-md")}>
 			<nav className="mx-auto grid w-full max-w-lg auto-cols-fr grid-flow-col">
@@ -32,37 +33,50 @@ export function MobileBottomNav() {
 				>
 					<TabNavItem icon={LucideDices} label="Dice" />
 				</ActiveLink>
-				{!isLoaded ? null : user ? (
-					<SignOutButton>
-						<button>
-							<TabNavItem icon={user.imageUrl} label="Sign Out" />
-						</button>
-					</SignOutButton>
-				) : (
-					<SignInButton>
-						<button>
-							<TabNavItem icon={LucideLogIn} label="Sign In" />
-						</button>
-					</SignInButton>
-				)}
+				<AuthButton />
 			</nav>
 		</footer>
 	)
 }
 
+function AuthButton() {
+	const { user, isLoaded } = useUser()
+	return !isLoaded ? (
+		<TabNavItem
+			icon={LucideLoader2}
+			iconClass="animate-spin"
+			label="Loading..."
+		/>
+	) : user ? (
+		<SignOutButton>
+			<button>
+				<TabNavItem icon={user.imageUrl} label="Sign Out" />
+			</button>
+		</SignOutButton>
+	) : (
+		<SignInButton>
+			<button>
+				<TabNavItem icon={LucideLogIn} label="Sign In" />
+			</button>
+		</SignInButton>
+	)
+}
+
 function TabNavItem({
 	icon: Icon,
+	iconClass,
 	label,
 }: {
 	icon: LucideIcon | string
+	iconClass?: string
 	label: string
 }) {
 	return (
 		<div className="flex flex-col items-center gap-1 py-2.5 text-center text-xs font-medium leading-none">
 			{typeof Icon === "string" ? (
-				<img src={Icon} className="rounded-full s-6" />
+				<img src={Icon} className={twMerge("rounded-full s-6", iconClass)} />
 			) : (
-				<Icon className="s-6" />
+				<Icon className={twMerge("s-6", iconClass)} />
 			)}
 			<span>{label}</span>
 		</div>
