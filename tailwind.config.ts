68e1a4b8553a13ceb0a33b129bbfd5856f26ba4e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-17 20:16:01 -0500

    add index.html to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import colors from "tailwindcss/colors"
 import plugin from "tailwindcss/plugin"
 
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}", "./index.html"],
 	theme: {
 		extend: {
 			colors: {
