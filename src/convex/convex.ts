1c5c3580901c64394c2399037201d19d32bca323
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-18 14:47:50 -0500

    make convex client in root again
    no need to export it; we should be able to only get it from context

--- src/convex/convex.ts
+++
@@ -1,4 +0,0 @@
-import { ConvexReactClient } from "convex/react"
-import { env } from "../env"
-
-export const convex = new ConvexReactClient(env.VITE_PUBLIC_CONVEX_URL)
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { ClerkProvider, useAuth } from "@clerk/clerk-react"
+import { ConvexReactClient } from "convex/react"
 import { ConvexProviderWithClerk } from "convex/react-clerk"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./app/App"
 import { LoadingSuspense } from "./app/LoadingPlaceholder"
-import { convex } from "./convex/convex"
 import { env } from "./env"
 import "./main.css"
+
+const convex = new ConvexReactClient(env.VITE_PUBLIC_CONVEX_URL)
 
 createRoot(document.querySelector("#root")!).render(
 	<ClerkProvider publishableKey={env.VITE_PUBLIC_CLERK_PUBLISHABLE_KEY}>
