6a7f05da525959a2045c1fc7c059e5e4a5632196
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-21 16:21:29 -0500

    slight optimization that makes me feel better

--- src/helpers/useMemoValue.ts
+++ src/helpers/useMemoValue.ts
@@ -1,12 +1,14 @@
 import isDeepEqual from "fast-deep-equal"
 import { useRef } from "react"
 
+const empty = Symbol()
+
 export function useMemoValue<T>(
 	value: T,
 	isEqual: (prev: T, next: T) => unknown = isDeepEqual,
 ): T {
-	const ref = useRef(value)
-	if (!isEqual(value, ref.current)) {
+	const ref = useRef<typeof empty | T>(empty)
+	if (ref.current === empty || !isEqual(value, ref.current)) {
 		ref.current = value
 	}
 	return ref.current
