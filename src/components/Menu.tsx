a335489fa36345c72c7130af4e8748019663ef0b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-23 22:03:24 -0500

    add menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,40 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { panel } from "../styles/panel.ts"
+
+export const Menu = DropdownMenu.Root
+export const MenuTrigger = DropdownMenu.Trigger
+
+export function MenuPanel({
+	children,
+	...props
+}: {
+	side: NonNullable<DropdownMenu.DropdownMenuContentProps["side"]>
+	align: NonNullable<DropdownMenu.DropdownMenuContentProps["align"]>
+	children: React.ReactNode
+}) {
+	return (
+		<DropdownMenu.Portal>
+			<DropdownMenu.Content
+				{...props}
+				className={panel(
+					"min-w-32 overflow-clip rounded-md border shadow-md focus-visible:ring-0",
+				)}
+				sideOffset={8}
+			>
+				{children}
+			</DropdownMenu.Content>
+		</DropdownMenu.Portal>
+	)
+}
+
+export function MenuItem(props: DropdownMenu.DropdownMenuItemProps) {
+	return (
+		<DropdownMenu.Item
+			{...props}
+			className={panel(
+				"flex cursor-pointer gap-2 px-3 py-2 transition focus-visible:ring-0 data-[highlighted]:bg-base-800",
+				props.className,
+			)}
+		/>
+	)
+}
