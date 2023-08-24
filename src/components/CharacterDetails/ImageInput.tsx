4874338067527c96c553186b737f51feb679fd04
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-24 01:01:37 -0500

    image input tweaks

--- src/components/CharacterDetails/ImageInput.tsx
+++ src/components/CharacterDetails/ImageInput.tsx
@@ -9,23 +9,14 @@ export function ImageInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
 				{...props}
 				className="h-10 rounded-t-md bg-transparent px-3 leading-none"
 			/>
-			<div className="group relative aspect-square">
+			<div className="aspect-square">
 				{typeof props.value === "string" && isUrl(props.value) && (
-					<a
-						href={props.value}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="absolute inset-0 overflow-clip rounded-b-md"
-					>
+					<a href={props.value} target="_blank" rel="noopener noreferrer">
 						<img
+							key={props.value}
 							src={props.value}
 							alt=""
-							className="absolute inset-0 object-cover transition s-full group-hover:brightness-50 group-focus:brightness-50"
-						/>
-						<img
-							src={props.value}
-							alt=""
-							className="absolute inset-0 object-contain opacity-0 transition s-full group-hover:opacity-100 group-focus:opacity-100"
+							className="aspect-square object-contain"
 						/>
 					</a>
 				)}
