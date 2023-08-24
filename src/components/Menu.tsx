4930cca1c6d7bc55e5d5e3935bcbc7d739d4eefc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-23 22:09:15 -0500

    menu style tweak

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -32,7 +32,7 @@ export function MenuItem(props: DropdownMenu.DropdownMenuItemProps) {
 		<DropdownMenu.Item
 			{...props}
 			className={panel(
-				"flex cursor-pointer gap-2 px-3 py-2 transition focus-visible:ring-0 data-[highlighted]:bg-base-800",
+				"flex cursor-pointer items-center gap-2 px-2 py-2 transition focus-visible:ring-0 data-[highlighted]:bg-base-800",
 				props.className,
 			)}
 		/>
