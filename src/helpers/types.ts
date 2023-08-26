2a8ec3bba6cb135f567a5becdf42f7351630157f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-25 19:33:02 -0500

    add back Nullish type

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -5,3 +5,5 @@ export type Falsy = false | 0 | "" | null | undefined
 export type Spread<A extends object, B extends object> = Omit<A, keyof B> & B
 
 export type Simplify<T> = { [K in keyof T]: T[K] } & NonNullable<unknown>
+
+export type Nullish<T> = T | null | undefined
