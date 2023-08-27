63f0b4b437e4d63bd719f99d1c321e07c4724af4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-27 13:59:42 -0500

    menu adjustments

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,25 +4,17 @@ import { panel } from "../styles/panel.ts"
 export const Menu = DropdownMenu.Root
 export const MenuTrigger = DropdownMenu.Trigger
 
-export function MenuPanel({
-	children,
-	...props
-}: {
-	side: NonNullable<DropdownMenu.DropdownMenuContentProps["side"]>
-	align: NonNullable<DropdownMenu.DropdownMenuContentProps["align"]>
-	children: React.ReactNode
-}) {
+export function MenuPanel(props: DropdownMenu.DropdownMenuContentProps) {
 	return (
 		<DropdownMenu.Portal>
 			<DropdownMenu.Content
+				sideOffset={8}
 				{...props}
 				className={panel(
 					"min-w-32 overflow-clip rounded-md border shadow-md focus-visible:ring-0",
+					props.className,
 				)}
-				sideOffset={8}
-			>
-				{children}
-			</DropdownMenu.Content>
+			/>
 		</DropdownMenu.Portal>
 	)
 }
@@ -32,7 +24,7 @@ export function MenuItem(props: DropdownMenu.DropdownMenuItemProps) {
 		<DropdownMenu.Item
 			{...props}
 			className={panel(
-				"flex cursor-pointer items-center gap-2 px-2 py-2 transition focus-visible:ring-0 data-[highlighted]:bg-base-800",
+				"flex w-full cursor-pointer items-center gap-2 px-2 py-2 transition focus-visible:ring-0 data-[highlighted]:bg-base-800",
 				props.className,
 			)}
 		/>
