50eca8bd291c70d4700b3bdc0b18de7b8105199a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-27 13:35:32 -0500

    add AsyncButton

---
+++ src/components/AsyncButton.tsx
@@ -0,0 +1,33 @@
+import { type ComponentPropsWithRef } from "react"
+import { twMerge } from "tailwind-merge"
+import { autoRef } from "../helpers/autoRef.ts"
+import { type Spread } from "../helpers/types.ts"
+import { useAsyncCallback } from "../helpers/useAsyncCallback.ts"
+
+export const AsyncButton = autoRef(function AsyncButton({
+	onClick,
+	...props
+}: Spread<
+	ComponentPropsWithRef<"button">,
+	{
+		onClick: (event: React.MouseEvent) => PromiseLike<unknown>
+	}
+>) {
+	const [handleClick, state] = useAsyncCallback(
+		async (event: React.MouseEvent<HTMLButtonElement>) => {
+			try {
+				await onClick(event)
+			} catch (error) {
+				console.error(error)
+			}
+		},
+	)
+	return (
+		<button
+			{...props}
+			onClick={handleClick}
+			disabled={props.disabled ?? state.isLoading}
+			className={twMerge("disabled:opacity-50", props.className)}
+		/>
+	)
+})
