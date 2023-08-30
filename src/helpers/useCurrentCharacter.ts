42f8eb50ca1b77979225cd2dcf528292a15694dd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-08-30 18:05:46 -0500

    add horrifying useQueriesSuspense

--- src/helpers/useCurrentCharacter.ts
+++ src/helpers/useCurrentCharacter.ts
@@ -1,15 +1,21 @@
 import { api } from "convex/_generated/api.js"
 import { useAppParams } from "./useAppParams.ts"
-import { useQuerySuspense } from "./useQuerySuspense.ts"
+import { useQueriesSuspense } from "./useQuerySuspense.ts"
 
 export function useCurrentCharacter() {
 	const params = useAppParams()
 	const characterId = params.characterId.current
-	const characters = useQuerySuspense(api.characters.list)
-	const loadedCharacter = useQuerySuspense(api.characters.get, {
-		id: characterId,
-	})
-	const assignedCharacter = useQuerySuspense(api.characters.getOwned)
+
+	const { characters, loadedCharacter, assignedCharacter } = useQueriesSuspense(
+		{
+			characters: { query: api.characters.list, args: {} },
+			loadedCharacter: {
+				query: api.characters.get,
+				args: { id: characterId },
+			},
+			assignedCharacter: { query: api.characters.getOwned, args: {} },
+		},
+	)
 
 	if (!characterId) {
 		return assignedCharacter ?? characters[0]
--- src/helpers/useQuerySuspense.ts
+++ src/helpers/useQuerySuspense.ts
@@ -1,11 +1,14 @@
-import { useConvex, useQuery } from "convex/react"
+import { useConvex, useQueries, useQuery } from "convex/react"
 import {
 	type FunctionReference,
 	type FunctionReturnType,
 	type OptionalRestArgs,
 	getFunctionName,
 } from "convex/server"
+import isEqual from "fast-deep-equal"
 import { LRUCache } from "lru-cache"
+import { RequestForQueries } from "../../node_modules/.pnpm/convex@1.1.1_@clerk+clerk-react@4.24.0_react-dom@18.2.0_react@18.2.0/node_modules/convex/src/react/use_queries"
+import { useMemoValue } from "./useMemoValue.ts"
 
 const cache = new LRUCache<string, NonNullable<unknown>>({
 	max: 100,
@@ -69,4 +72,72 @@ export function useQuerySuspense<Query extends FunctionReference<"query">>(
 
 	const queryData = useQuery(query, ...args)
 	return queryData === undefined ? cacheData : queryData
+}
+
+export function useQueriesSuspense<Queries extends RequestForQueries>(
+	queries: Queries,
+): { [K in keyof Queries]: FunctionReturnType<Queries[K]["query"]> } {
+	const convex = useConvex()
+	const results: Record<string, unknown> = {}
+	const promises: Promise<void>[] = []
+
+	for (const [key, { query, args }] of Object.entries(queries)) {
+		const cacheData = getQueryCacheData(query, [args])
+
+		if (cacheData !== undefined) {
+			results[key] = cacheData
+			continue
+		}
+
+		const promise = new Promise<void>((resolve) => {
+			const watch = convex.watchQuery(query, args)
+
+			const result = watch.localQueryResult()
+			if (result !== undefined) {
+				setQueryCacheData(query, [args], result)
+				resolve()
+				return
+			}
+
+			// rome-ignore lint/correctness/noUnusedVariables: false positive
+			const unsubscribe = watch.onUpdate(() => {
+				const result = watch.localQueryResult()
+				if (result === undefined) {
+					throw new Error("No query result")
+				}
+
+				setQueryCacheData(query, [args], result)
+				resolve()
+				unsubscribe()
+			})
+		})
+		promises.push(promise)
+	}
+
+	if (promises.length > 0) {
+		throw Promise.all(promises)
+	}
+
+	const memoQueries = useMemoValue(queries, (prev, next) => {
+		const prevEntries = Object.entries(prev).map(([key, { query, args }]) => [
+			key,
+			{ query: getFunctionName(query), args },
+		])
+		const nextEntries = Object.entries(next).map(([key, { query, args }]) => [
+			key,
+			{ query: getFunctionName(query), args },
+		])
+		return isEqual(prevEntries, nextEntries)
+	})
+
+	const queryResults = useQueries(memoQueries)
+	for (const [key, queryResult] of Object.entries(queryResults)) {
+		if (queryResult !== undefined) {
+			results[key] = queryResult
+		}
+	}
+
+	return results as {
+		[K in keyof Queries]: FunctionReturnType<Queries[K]["query"]>
+	}
 }
