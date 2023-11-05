84100949a4b33b879917b033277fb3a79cf2e6a1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-05 13:23:06 -0600

    use one env.d.ts file

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,9 @@
+import "@total-typescript/ts-reset"
+import "vite/client"
+
 declare global {
 	interface ImportMetaEnv {
 		VITE_PUBLIC_CONVEX_URL: string
 		VITE_PUBLIC_CLERK_PUBLISHABLE_KEY: string
 	}
 }
-export {}
