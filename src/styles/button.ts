b2027d134db9ddc65251cc424330e3d1da073d28
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-05 20:19:30 -0600

    use focus visible

--- src/styles/button.ts
+++ src/styles/button.ts
@@ -1,5 +1,5 @@
 import { twStyle } from "./twStyle.ts"
 
 export const solidButton = twStyle(
-	"inline-flex min-h-10 items-center justify-center gap-3 rounded-md border border-accent-700 bg-accent-700 bg-opacity-25 px-3 py-1 leading-tight text-white ring-accent-300 transition hover:bg-opacity-50 focus:outline-none focus:ring-2 [&>svg]:-mx-1 [&>svg]:flex-shrink-0 [&>svg]:s-5",
+	"inline-flex min-h-10 items-center justify-center gap-3 rounded-md border border-accent-700 bg-accent-700 bg-opacity-25 px-3 py-1 leading-tight text-white ring-accent-300 transition hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 [&>svg]:-mx-1 [&>svg]:flex-shrink-0 [&>svg]:s-5",
 )
