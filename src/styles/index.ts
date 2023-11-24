cc29580ef306faaa5184611c09a031d99096d972
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-23 21:35:41 -0600

    input transitions

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,9 +4,11 @@ import { twStyle } from "./twStyle.ts"
 export const center = twStyle("flex items-center justify-center")
 
 export const input = twStyle(
-	panel("h-10 w-full min-w-0 rounded-md border p-3 leading-none"),
+	panel("h-10 w-full min-w-0 rounded-md border p-3 leading-none transition"),
 )
 
 export const textArea = twStyle(
-	panel("w-full min-w-0 resize-none rounded-md border px-3 py-2 leading-6"),
+	panel(
+		"w-full min-w-0 resize-none rounded-md border px-3 py-2 leading-6 transition",
+	),
 )
