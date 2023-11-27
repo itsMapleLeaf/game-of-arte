2c62204da64a13f2fa5d30b8644ed5cf6d05410a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-27 01:08:57 -0600

    change boost dice activation to 1 in 3

--- src/features/dice/constants.ts
+++ src/features/dice/constants.ts
@@ -1,4 +1,4 @@
 export const ACTION_DICE_SIDES = 12
 
-export const MODIFIER_DICE_SIDES = 4
-export const MODIFIER_DICE_THRESHOLD = 4
+export const MODIFIER_DICE_SIDES = 6
+export const MODIFIER_DICE_THRESHOLD = 5
