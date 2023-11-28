9b4824873d7720412aa7533f6658c3ac30a67e38
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-28 00:22:03 -0600

    improve attributes on character data

--- src/features/characters/useCharacterDataValue.ts
+++ src/features/characters/useCharacterDataValue.ts
@@ -1,32 +1,16 @@
-import { api } from "convex/_generated/api"
 import type { Doc } from "convex/_generated/dataModel.js"
-import { useMutation } from "convex/react"
 import { type CharacterData, parseCharacterData } from "./data.ts"
+import { useUpdateCharacterData } from "./useUpdateCharacterData.ts"
 
 export function useCharacterDataValue(
 	character: Doc<"characters">,
 	key: keyof CharacterData,
 ) {
 	const value = parseCharacterData(character.data)[key]
-
-	const update = useMutation(api.characters.updateData).withOptimisticUpdate(
-		(store, args) => {
-			const character = store.getQuery(api.characters.get, { id: args.id })
-			if (!character) return
-
-			store.setQuery(
-				api.characters.get,
-				{ id: character._id },
-				{
-					...character,
-					data: { ...character.data, ...args.data },
-				},
-			)
-		},
-	)
+	const update = useUpdateCharacterData()
 
 	function setValue(newValue: string | number) {
-		void update({ id: character._id, data: { [key]: newValue } })
+		void update(character._id, { [key]: newValue })
 	}
 
 	return [value, setValue] as const
