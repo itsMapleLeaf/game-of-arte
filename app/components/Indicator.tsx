b26bd977441135eda333c3fea8937098a46fede4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-29 17:38:26 -0600

    fix dom nesting warning

--- app/components/Indicator.tsx
+++ app/components/Indicator.tsx
@@ -19,7 +19,7 @@ export function Indicator({
 	return (
 		<Tooltip disableHoverableContent>
 			<TooltipTrigger tabIndex={0} asChild>
-				<p
+				<span
 					className={twMerge(
 						"-mx-2 -my-1.5 grid cursor-default grid-cols-[1.5rem,auto] items-center gap-1 rounded-md px-2 py-1.5 tabular-nums transition hover:brightness-150",
 						className,
@@ -27,7 +27,7 @@ export function Indicator({
 				>
 					<span aria-hidden>{icon}</span>
 					{value}
-				</p>
+				</span>
 			</TooltipTrigger>
 			<TooltipContent>
 				{value} {label}
