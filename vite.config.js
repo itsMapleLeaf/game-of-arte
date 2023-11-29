60d3a755db0b7bae5754de16b9d3234735e32657
itsMapleLeaf <itsMapleLeaf@users.noreply.github.com>
2023-11-29 03:18:11 +0000

    Apply automatic changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import { unstable_vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-import { unstable_vitePlugin as remix } from "@remix-run/dev"
 
 export default defineConfig({
 	plugins: [
