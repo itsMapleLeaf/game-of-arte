b400ff9639f4c953f7ec3055c4d4eb8eb453dc8c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-29 17:48:40 -0600

    put all meta tags in meta function

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,11 @@ import { AuthGuard } from "./features/auth/AuthGuard.tsx"
 
 const convex = new ConvexReactClient(env.VITE_PUBLIC_CONVEX_URL)
 
-export const meta: MetaFunction = () => [{ title: "Game of Arte" }]
+export const meta: MetaFunction = () => [
+	{ title: "Game of Arte" },
+	{ charSet: "utf-8" },
+	{ name: "viewport", content: "width=device-width, initial-scale=1.0" },
+]
 
 export const links: LinksFunction = () => [{ rel: "icon", href: faviconUrl }]
 
@@ -32,8 +36,6 @@ export default function Root() {
 			className="break-words bg-base-950 text-base-100 [&_*::-webkit-scrollbar-thumb]:bg-base-700 [&_*::-webkit-scrollbar-track]:bg-base-900 [&_*::-webkit-scrollbar]:w-2"
 		>
 			<head>
-				<meta charSet="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<Meta />
 				<Links />
 			</head>
