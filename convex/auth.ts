0a5332dc2fa4fa22197ae3b4bc671c2548094ff4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-29 21:04:56 -0600

    identify via clerk webhook

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,52 +1,46 @@
+import type { WebhookEvent } from "@clerk/remix/api.server"
+import { createClerkClient } from "@clerk/remix/api.server"
+import { Webhook } from "svix"
 import { internal } from "./_generated/api.js"
-import { action } from "./_generated/server.js"
+import { httpAction } from "./_generated/server.js"
 
-export const identify = action({
-	handler: async (ctx) => {
-		const identity = await ctx.auth.getUserIdentity()
-		if (!identity) return null
+export const clerkUserWebhook = httpAction(async (ctx, request) => {
+	const webhook = new Webhook(process.env.WEBHOOK_SECRET as string)
 
-		const response = await fetch(
-			`https://api.clerk.com/v1/users/${identity.subject}`,
-			{
-				headers: { Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}` },
-			},
-		)
-		if (!response.ok) {
-			console.error(await response.json().catch(() => "Unknown error"))
-			throw new Error("Failed to fetch user info")
-		}
+	const event = webhook.verify(
+		await request.text(),
+		Object.fromEntries(request.headers),
+	) as WebhookEvent
+	if (
+		event.type !== "user.created" &&
+		event.type !== "user.updated" &&
+		event.type !== "session.created"
+	) {
+		throw new Error("Unexpected event type")
+	}
 
-		const clerkUser = (await response.json()) as {
-			external_accounts: Array<{
-				provider: string
-				provider_user_id: string
-			}>
-		}
+	const clerk = createClerkClient({
+		secretKey: process.env.CLERK_SECRET_KEY as string,
+	})
 
-		const discordAccount = clerkUser.external_accounts.find(
-			(a) => a.provider === "oauth_discord",
-		)
-		if (!discordAccount) {
-			throw new Error("Discord account not found")
-		}
+	const user = await clerk.users.getUser(
+		event.type === "session.created" ? event.data.user_id : event.data.id,
+	)
 
-		const name =
-			identity.name || identity.nickname || identity.preferredUsername
+	const discordUserId = user.externalAccounts.find(
+		(a) => a.provider === "oauth_discord",
+	)?.externalId
+	if (!discordUserId) {
+		throw new Error("Discord account not found")
+	}
 
-		if (!name) {
-			console.warn("User has no name", {
-				name: identity.name,
-				nickname: identity.nickname,
-				preferredUsername: identity.preferredUsername,
-				tokenIdentifier: identity.tokenIdentifier,
-			})
-		}
+	await ctx.scheduler.runAfter(0, internal.users.update, {
+		name: user.username || "unnamed",
+		discordUserId,
 
-		await ctx.scheduler.runAfter(0, internal.users.update, {
-			name: name ?? "unnamed",
-			tokenIdentifier: identity.tokenIdentifier,
-			discordUserId: discordAccount.provider_user_id,
-		})
-	},
+		// dangerous implementation detail, but I don't know how else to get this :D
+		tokenIdentifier: `${process.env.CLERK_JWT_ISSUER_DOMAIN}|${user.id}`,
+	})
+
+	return new Response("OK", { status: 200 })
 })
