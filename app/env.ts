c6e5fa45090b5a4c0f83fc0f3603347d72e1588c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-11-30 21:05:47 -0600

    unused public env variable

--- app/env.ts
+++ app/env.ts
@@ -3,13 +3,10 @@ import { parseEnv } from "./helpers/env.ts"
 
 const schema = v.object({
 	VITE_PUBLIC_CONVEX_URL: v.string([v.minLength(1)]),
-	VITE_PUBLIC_CLERK_PUBLISHABLE_KEY: v.string([v.minLength(1)]),
 })
 
 const input: Partial<v.Input<typeof schema>> = {
 	VITE_PUBLIC_CONVEX_URL: import.meta.env.VITE_PUBLIC_CONVEX_URL,
-	VITE_PUBLIC_CLERK_PUBLISHABLE_KEY: import.meta.env
-		.VITE_PUBLIC_CLERK_PUBLISHABLE_KEY,
 }
 
 export const env = parseEnv(schema, input)
