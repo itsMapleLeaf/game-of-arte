a1bc5def17287d1ddf1ed0dc1472dea6a2e0d43b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-02 15:02:25 -0600

    use ref in useStableQuery again

--- app/helpers/useStableQuery.tsx
+++ app/helpers/useStableQuery.tsx
@@ -1,21 +1,14 @@
 import { useQuery } from "convex/react"
 import type { FunctionReference, OptionalRestArgs } from "convex/server"
-import { useState } from "react"
-import { useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect.tsx"
+import { useRef } from "react"
 
 export function useStableQuery<
 	FuncRef extends FunctionReference<"query", "public">,
 >(func: FuncRef, ...args: OptionalRestArgs<FuncRef>) {
 	const data = useQuery(func, ...args)
-	const [stableData, setStableData] = useState(data)
-
-	// technically we can do this without an effect,
-	// but forgetti refuses to update otherwise
-	useIsomorphicLayoutEffect(() => {
-		if (data !== stableData && data !== undefined) {
-			setStableData(data)
-		}
-	}, [data, stableData])
-
-	return stableData
+	const ref = useRef(data)
+	if (ref.current !== data && data !== undefined) {
+		ref.current = data
+	}
+	return ref.current
 }
