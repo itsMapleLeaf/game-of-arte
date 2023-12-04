08efba45d6a56e371ceafab6419c66eab7ad3e7b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-03 18:21:44 -0600

    new layout + placeholder spellbook page

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,58 +1,26 @@
-import { useUser } from "@clerk/remix"
 import { useSearchParams } from "@remix-run/react"
 import { api } from "convex/_generated/api.js"
 import type { Id } from "convex/_generated/dataModel.js"
-import { useConvexAuth } from "convex/react"
 import { LoadingPlaceholder } from "~/components/LoadingPlaceholder.tsx"
-import { AuthButton } from "~/features/auth/AuthButton.tsx"
 import { CharacterContext } from "~/features/characters/CharacterContext.tsx"
 import { CharacterDetails } from "~/features/characters/CharacterDetails.tsx"
 import { container } from "~/styles/container.ts"
-import { twMerge } from "~/styles/twMerge.ts"
 import { useStableQuery } from "../../helpers/useStableQuery.tsx"
 import { SideNav } from "./SideNav.tsx"
 import { ViewportHeightScrollArea } from "./ViewportHeightScrollArea.tsx"
 
 export default function GamePage() {
 	return (
-		<AuthLoadingCover>
-			<div className={container("flex min-h-[100dvh] flex-col gap-4 p-4")}>
-				<header className="flex h-12">
-					<div className="flex flex-1 items-center justify-end">
-						<AuthButton />
-					</div>
-				</header>
-				<div className="flex flex-1 gap-4">
-					<ViewportHeightScrollArea>
-						<nav className="flex flex-col gap-4">
-							<SideNav />
-						</nav>
-					</ViewportHeightScrollArea>
-					<main className="flex-1">
-						<MainContent />
-					</main>
-				</div>
-			</div>
-		</AuthLoadingCover>
-	)
-}
-
-function AuthLoadingCover({ children }: { children: React.ReactNode }) {
-	const clerkAuth = useUser()
-	const convexAuth = useConvexAuth()
-	const authLoading = !clerkAuth.isLoaded || convexAuth.isLoading
-	return (
-		<>
-			{children}
-			<LoadingPlaceholder
-				className={twMerge(
-					"fixed inset-0 bg-base-950 transition-all duration-300",
-					authLoading ? "visible opacity-100" : "invisible opacity-0",
-				)}
-			>
-				Just a moment...
-			</LoadingPlaceholder>
-		</>
+		<div className={container("flex flex-1 gap-4 pb-8 pt-4")}>
+			<ViewportHeightScrollArea>
+				<nav className="flex flex-col gap-4">
+					<SideNav />
+				</nav>
+			</ViewportHeightScrollArea>
+			<main className="flex-1">
+				<MainContent />
+			</main>
+		</div>
 	)
 }
 
