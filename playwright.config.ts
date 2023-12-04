026491430b15c63fae32a1ba3e308043a05f5492
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-03 20:31:51 -0600

    fix e2e setup
    ensures convex functions are actually deployed before the test is run

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "pnpm build && pnpm start",
+		command: "pnpm start",
 		url: "http://localhost:3000",
 		reuseExistingServer: !process.env.CI,
 	},
