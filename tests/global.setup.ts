51ae78228ce3f1189d375a46c906337b42bdc1cc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-03 22:19:29 -0600

    run seed separately in CI

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -1,7 +1,9 @@
 import { execa } from "execa"
 
 export default async function setup() {
-	await execa("pnpm", ["run", "seed"], {
-		stdio: "inherit",
-	})
+	if (!process.env.CI) {
+		await execa("pnpm", ["run", "seed"], {
+			stdio: "inherit",
+		})
+	}
 }
