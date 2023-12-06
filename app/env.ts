3b718c97c29b0cc81366346df0f611946f739b4f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-05 19:39:50 -0600

    fix type error

--- app/env.ts
+++ app/env.ts
@@ -5,7 +5,7 @@ const schema = z.object({
 	VITE_PUBLIC_CONVEX_URL: z.string().min(1),
 })
 
-const input: Partial<v.Input<typeof schema>> = {
+const input: Partial<z.input<typeof schema>> = {
 	VITE_PUBLIC_CONVEX_URL: import.meta.env.VITE_PUBLIC_CONVEX_URL,
 }
 
