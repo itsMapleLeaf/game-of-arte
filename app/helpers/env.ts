5625e86dfe7b685b8f12a0296efc2fd16b761aec
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-06 12:34:10 -0600

    externalize env test
    so convex can import it

--- app/helpers/env.ts
+++ app/helpers/env.ts
@@ -19,38 +19,3 @@ export function parseEnv<Env extends Record<string, string>>(
 
 	return result.data
 }
-
-if (import.meta.vitest) {
-	const { describe, expect, it } = import.meta.vitest
-
-	describe("parseEnv", () => {
-		it("returns the parsed environment variables", () => {
-			const schema = z.object({
-				FOO: z.string().min(1),
-				BAR: z.string().min(1),
-			})
-
-			const input = {
-				FOO: "foo",
-				BAR: "bar",
-			}
-
-			expect(parseEnv(schema, input)).toEqual(input)
-		})
-
-		it("throws an error if environment fails to parse", () => {
-			const schema = z.object({
-				FOO: z.string().min(1),
-				BAR: z.string().min(1),
-				BAZ: z.string().min(1),
-			})
-
-			const input = {
-				FOO: "foo",
-				BAR: "",
-			}
-
-			expect(() => parseEnv(schema, input)).toThrowError(/BAR[\s\S]+BAZ/)
-		})
-	})
-}
