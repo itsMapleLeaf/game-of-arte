961a3111d4c10c6930601915102088af682adb57
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-07 17:05:12 -0600

    spell table

--- app/routes/spellbook/route.tsx
+++ app/routes/spellbook/route.tsx
@@ -1,10 +1,114 @@
-import { LucideConstruction } from "lucide-react"
-import { EmptyState } from "~/components/EmptyState.tsx"
+import { api } from "convex/_generated/api.js"
+import { useQuery } from "convex/react"
+import { LucideSparkles } from "lucide-react"
+import type { Key, ReactNode } from "react"
+import { getAttributeById } from "~/features/characters/attributes.ts"
+import { sorcerySpells } from "~/features/sorcery/spells.ts"
+import { plural } from "~/helpers/string.ts"
+import { container } from "~/styles/container.ts"
+import { panel } from "~/styles/panel.ts"
 
 export default function SpellbookPage() {
+	const character = useQuery(api.characters.getOwned)
+
+	type Row = {
+		id: Key
+		name: ReactNode
+		attribute: ReactNode
+		cost: ReactNode
+		castingTime: ReactNode
+		description: ReactNode
+		amplifiedDescription: ReactNode
+		drawbacks: string[]
+		freeform?: boolean
+	}
+
+	const rows: Row[] = [
+		...Object.entries(sorcerySpells).map(([id, spell]) => ({
+			id,
+			name: spell.name,
+			attribute: getAttributeById(spell.attributeId).name,
+			cost: `${spell.cost.mana} mana${
+				spell.cost.mentalStress ? `, ${spell.cost.mentalStress} stress` : ""
+			}`,
+			castingTime:
+				spell.castingTime?.turns ?
+					plural(spell.castingTime.turns, `turn`)
+				:	"Immediate",
+			description: spell.description,
+			amplifiedDescription: spell.amplifiedDescription,
+			drawbacks: spell.drawbacks ?? [],
+		})),
+		...(character?.freeformSpells?.map((spell) => ({
+			id: spell.id,
+			name: spell.name,
+			attribute: getAttributeById(spell.attributeId).name,
+			cost: `${spell.cost.mana} mana${
+				spell.cost.mentalStress ? `, ${spell.cost.mentalStress} stress` : ""
+			}`,
+			castingTime:
+				spell.castingTime?.turns ?
+					plural(spell.castingTime.turns, `turn`)
+				:	"Immediate",
+			description: spell.description,
+			amplifiedDescription: spell.amplifiedDescription,
+			drawbacks: spell.drawbacks ?? [],
+			freeform: true,
+		})) ?? []),
+	]
+		.sort((a, b) =>
+			a.name.toLocaleLowerCase().localeCompare(b.name.toLocaleLowerCase()),
+		)
+		.sort((a, b) => a.attribute.localeCompare(b.attribute.toLocaleLowerCase()))
+
 	return (
-		<EmptyState icon={LucideConstruction} className="py-16">
-			Under construction. Check back later!
-		</EmptyState>
+		<main className={container("max-w-screen-xl py-4")}>
+			<table
+				className={panel(
+					"grid grid-cols-[auto,auto,auto,auto,2fr,1.5fr,1.5fr] gap-2 rounded-md bg-base-950 [&_:is(th,td)]:px-3 [&_:is(th,td)]:py-2 [&_:is(th,td)]:text-start [&_th]:font-medium",
+				)}
+			>
+				<thead className="contents">
+					<tr className="col-span-full grid grid-cols-[subgrid]">
+						<th>Name</th>
+						<th>Attribute</th>
+						<th>Cost</th>
+						<th>Casting Time</th>
+						<th>Description</th>
+						<th>Amplified</th>
+						<th>Drawbacks</th>
+					</tr>
+				</thead>
+				<tbody className="contents">
+					{rows.map((row) => (
+						<tr
+							key={row.id}
+							className={panel(
+								"col-span-full grid grid-cols-[subgrid] rounded border border-base-800 bg-base-900",
+							)}
+						>
+							<td>
+								{row.name}{" "}
+								{row.freeform && <LucideSparkles className="inline s-[1em]" />}
+							</td>
+							<td>{row.attribute}</td>
+							<td>{row.cost}</td>
+							<td>{row.castingTime}</td>
+							<td>{row.description}</td>
+							<td>{row.amplifiedDescription}</td>
+							<td>
+								{row.drawbacks && (
+									<ul className="list-disc">
+										{row.drawbacks.map((drawback) => (
+											<li key={drawback}>{drawback}</li>
+										))}
+									</ul>
+								)}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</main>
 	)
 }
