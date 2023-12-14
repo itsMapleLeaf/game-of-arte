9b46e504f805997dc27e59c1f710cabc2ac51367
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-13 18:15:38 -0600

    pick: return simplified type

--- app/helpers/object.ts
+++ app/helpers/object.ts
@@ -1,7 +1,9 @@
+import type { Simplify } from "./types.ts"
+
 export function pick<T extends object, K extends keyof T>(
 	obj: T,
 	keys: K[],
-): Pick<T, K> {
+): Simplify<Pick<T, K>> {
 	const result = {} as Pick<T, K>
 	for (const key of keys) {
 		result[key] = obj[key]
