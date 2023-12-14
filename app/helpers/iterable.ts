a3d1762ab66fe23586434d59cd4f9b282e7a2ab9
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-14 12:27:52 -0600

    iterable fun

--- app/helpers/iterable.ts
+++ app/helpers/iterable.ts
@@ -7,21 +7,53 @@ class ExtendedIterable<T> implements Iterable<T> {
 		yield* this.iterable
 	}
 
+	array() {
+		return [...this]
+	}
+
+	entries() {
+		return this.map((value, index) => [index, value] as const)
+	}
+
+	first() {
+		const iterable = this[Symbol.iterator]().next()
+		return iterable.done ? undefined : iterable.value
+	}
+
+	find(predicate: BooleanConstructor): Truthy<T> | undefined
+	find(predicate: (value: T) => value is T): T | undefined
+	find(predicate: (value: T) => boolean): T | undefined
+	find(predicate: (value: T) => boolean) {
+		return this.accept(predicate).first()
+	}
+
+	promiseAll() {
+		return Promise.all(this)
+	}
+
+	toMap<K, V>(fn: (value: T, index: number) => [key: K, value: V]) {
+		return new Map(this.map(fn))
+	}
+
 	apply<U>(fn: (iterable: ExtendedIterable<T>) => Iterable<U>) {
 		return new ExtendedIterable(fn(this))
 	}
 
-	map<U>(mapper: (value: T) => U) {
+	map<U>(mapper: (value: T, index: number) => U) {
 		return this.apply(function* (iterable: ExtendedIterable<T>) {
+			let index = 0
 			for (const value of iterable) {
-				yield mapper(value)
+				yield mapper(value, index)
+				index += 1
 			}
 		})
 	}
 
-	filter(filter: BooleanConstructor): ExtendedIterable<Truthy<T>>
-	filter<U extends T>(filter: (value: T) => value is U): ExtendedIterable<U>
-	filter(filter: (value: T) => boolean) {
+	// @ts-expect-error: don't know why this is yelling lol
+	accept(filter: BooleanConstructor): ExtendedIterable<Truthy<T>>
+	accept<U extends T>(filter: (value: T) => value is U): ExtendedIterable<U>
+	accept(filter: (value: T) => boolean): ExtendedIterable<T>
+	accept(filter: (value: T) => boolean): ExtendedIterable<T> {
 		return this.apply(function* (iterable: ExtendedIterable<T>) {
 			for (const value of iterable) {
 				if (filter(value)) yield value
@@ -31,14 +63,10 @@ class ExtendedIterable<T> implements Iterable<T> {
 
 	reduce<U>(reducer: (accumulator: U, value: T) => U, initialValue: U) {
 		let result = initialValue
-		for (const value of this.iterable) {
+		for (const value of this) {
 			result = reducer(result, value)
 		}
 		return result
-	}
-
-	array() {
-		return [...this.iterable]
 	}
 
 	unique() {
@@ -53,10 +81,15 @@ class ExtendedIterable<T> implements Iterable<T> {
 		})
 	}
 }
+export type { ExtendedIterable }
 
 /** Helper to wrap an iterator with useful methods */
 export function it<T>(iterable: Iterable<T>) {
 	return new ExtendedIterable(iterable)
+}
+
+it.objectEntries = function objectEntries<T>(obj: Record<string, T>) {
+	return it(Object.entries(obj))
 }
 
 it.factory = function factory<Args extends unknown[], Return>(
