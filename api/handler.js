8268a01e596c1e991d3b7e4b93483b3e7eaf9df2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-13 18:47:03 -0600

    update build paths

--- api/handler.js
+++ api/handler.js
@@ -1,5 +1,5 @@
 import { createRequestHandler } from "@vercel/remix/server"
-import * as build from "../build/index.js"
+import * as build from "../build/server/index.js"
 
 export default createRequestHandler({
 	build: { ...build, mode: process.env.NODE_ENV },
