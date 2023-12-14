0e5b8879b9e90ff79773caa89afd29b016e96c93
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-12-14 10:29:09 -0600

    remove guideLinks

--- app/features/guide/data.ts
+++ app/features/guide/data.ts
@@ -1,4 +1,3 @@
-import { startCase } from "lodash-es"
 import type { MDXModule } from "mdx/types"
 import { createDocumentMap } from "~/features/wiki/documents.tsx"
 
@@ -6,11 +5,3 @@ export const guideDocuments = createDocumentMap({
 	...import.meta.glob<MDXModule>("./documents/*.md"),
 	...import.meta.glob<MDXModule>("./documents/*.mdx"),
 })
-
-export const guideLinks = Object.entries(guideDocuments).map(
-	([key, document]) => ({
-		label: startCase(key.split(/[^a-z]/i).join(" ")),
-		icon: document.icon,
-		href: `/guide/${key}`,
-	}),
-)
